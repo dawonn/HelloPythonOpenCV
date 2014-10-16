cd5631451ca7e6f788eeeadf305e57c7fea27aae
Dereck Wonnacott <dereck@gmail.com>
2014-10-16 19:04:24 -0400

    Added function to record to video file

--- src/helloworld/Hello.py
+++ src/helloworld/Hello.py
@@ -4,10 +4,12 @@ Created on Oct 14, 2014
 @author: Dereck
 '''
 import cv2
+from cv2 import imshow
 
 if __name__ == '__main__':
     print('Hello World')
     
+    # Connect to video Source
     cam = cv2.VideoCapture()
     cam.open("http://10.25.86.11/mjpg/video.mjpg")
     if cam.isOpened():
@@ -15,7 +17,20 @@ if __name__ == '__main__':
     else:
         print("Failed to connect to the camera.")
         exit(-1)
+    
+    # Configure recording location
+    fourcc = cv2.cv.CV_FOURCC(*'XVID')
+    rec = cv2.VideoWriter('c:\users\dereck\desktop\output.avi', fourcc, 20.0, (800, 600))
+    
+    # Grab frames
+    while(True):
+        ret, frame = cam.read()            
+        imshow("Blah", frame)        
+        rec.write(frame)
         
-        
-        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
                 
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
